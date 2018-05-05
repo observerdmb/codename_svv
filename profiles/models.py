c9b7bbd45b3017fecc41e37408f370f6bd46996b
observerdmb <vasyarus@inbox.ru>
2018-05-06 02:31:03 +0300

    Test model is created, media_root is included. Worked.

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    login_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=15, verbose_name='Имя')
+    last_name = models.CharField(max_length=15, verbose_name='Фамилия')
+    photo = models.ImageField(upload_to='profiles/{0}/photo'.format(User.username), verbose_name='Фото')
+
+
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def empty_view(request):
+    template = 'profile.html'
+    context = {}
+    return render(request=request, template_name=template, context=context)
+
 # Create your views here.
