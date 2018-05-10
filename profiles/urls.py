b85a09fae3f263ad5047853dd868412013ecc7cd
observerdmb <vasyarus@inbox.ru>
2018-05-10 03:01:38 +0300

    div layout

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     url('^main/', views.my_page, name='main_page'),
     url('^not_exists/', views.empty_view, name='empty_page'),
+    url('', RedirectView.as_view(url='main/'))
 ]
